f42342f68ea41bd8db14a29a7e1dcb8eb52b2374
Athena <athena.ftw@hotmail.com>
2013-10-07 16:45:01 -0400

    Fixed cylinder, somewhat

--- scene_student/src/cs4620/shape/Cylinder.java
+++ scene_student/src/cs4620/shape/Cylinder.java
@@ -27,8 +27,8 @@ public class Cylinder extends TriangleMesh
 		
 		float[] vertexCoords = new float[numPoints*3];
 		float[] normalCoords = new float[numPoints*3];
-		int[] triangleVerts = new int[numPoints*3*2];
-		int[] wireframeVerts = new int[numPoints*2*2];
+		int[] triangleVerts = new int[(numPoints*3 + numPoints*3)*2];
+		int[] wireframeVerts = new int[(numPoints*2 + numPoints)*2];
 		
 		float x = 0;
 		float y = 0;
@@ -37,29 +37,29 @@ public class Cylinder extends TriangleMesh
 		int rowPos = 0;
 		
 		//Center point for the top face
-		vertexCoords[0] = 0;
-		vertexCoords[1] = 1;
-		vertexCoords[2] = 0;
+		vertexCoords[(numPoints*3)-6] = 0;
+		vertexCoords[(numPoints*3)-5] = 1;
+		vertexCoords[(numPoints*3)-4] = 0;
 		
 		//Center point for the bottom face
-		vertexCoords[(rowLength)*3] = 0;
-		vertexCoords[(rowLength)*3 +2] = -1;
-		vertexCoords[(rowLength)*3 +2] = 0;
+		vertexCoords[(numPoints*3)-3] = 0;
+		vertexCoords[(numPoints*3)-2] = -1;
+		vertexCoords[(numPoints*3)-1] = 0;
 		
 		//Normal for the top face center point
-		normalCoords[0] = 0;
-		normalCoords[1] = 1;
-		normalCoords[2] = 0;
+		normalCoords[(numPoints*3)-6] = 0;
+		normalCoords[(numPoints*3)-5] = 1;
+		normalCoords[(numPoints*3)-4] = 0;
 		
 		//Normal for the bottom face center point
-		normalCoords[(rowLength+1)*3] = 0;
-		normalCoords[(rowLength+1)*3+1] = -1;
-		normalCoords[(rowLength+1)*3+2] = 0;
+		normalCoords[(numPoints*3)-3] = 0;
+		normalCoords[(numPoints*3)-2] = -1;
+		normalCoords[(numPoints*3)-1] = 0;
 		
 		//Populate the points and normals for the top and bottom faces
 		for (float u = 0; u <= rowLength; u++) {
 			//Top face points
-			rowPos = (int)((u+1)*3);
+			rowPos = (int)(u*3);
 			
 			x = (float)(Math.cos((u/rowLength) * 2 * Math.PI));
 			y = (float)1;
@@ -74,12 +74,10 @@ public class Cylinder extends TriangleMesh
 			normalCoords[rowPos+1] = 1;
 			normalCoords[rowPos+2] = 0;
 			
-			//System.out.println("rowLength: " + rowLength + "; rowPos: " + rowPos);
-			
 			//Bottom face points
 			rowPos = (int)((u + rowLength + 1)*3);
 
-			y = (float)-1;
+			y = (float)(-1);
 			
 			vertexCoords[rowPos] = x;
 			vertexCoords[rowPos+1] = y;
@@ -96,34 +94,52 @@ public class Cylinder extends TriangleMesh
 			//Top face triangles
 			rowPos = (int)(u*3);
 			
-			triangleVerts[rowPos] = 0;
-			triangleVerts[rowPos+1] = u+1;
-			triangleVerts[rowPos+2] = u+2;
+			triangleVerts[rowPos] = (2*(rowLength+1));
+			triangleVerts[rowPos+1] = u;
+			triangleVerts[rowPos+2] = u+1;
 			
 			//Bottom face triangles
-			rowPos = ((u + rowLength)*3);
+			rowPos = (u + rowLength+1)*3;
 			
-			triangleVerts[rowPos] = rowLength + 1;
-			triangleVerts[rowPos+1] = u + rowLength + 3;
-			triangleVerts[rowPos+2] = u + rowLength + 4;
+			triangleVerts[rowPos] = (2*(rowLength+1))+1;
+			triangleVerts[rowPos+1] = u + rowLength + 1;
+			triangleVerts[rowPos+2] = u + rowLength + 2;
 			
 			//Top face lines
 			int vertPos = (u*4);
 			
-			wireframeVerts[vertPos] = 0;
-			wireframeVerts[vertPos+1] = u+1;
+			wireframeVerts[vertPos] = (2*(rowLength+1));
+			wireframeVerts[vertPos+1] = u;
 				
-			wireframeVerts[vertPos+2] = u+1;
-			wireframeVerts[vertPos+3] = u+2;
+			wireframeVerts[vertPos+2] = u;
+			wireframeVerts[vertPos+3] = u+1;
 			
 			//Bottom face lines
-			vertPos = (u + rowLength)*4;
+			vertPos = (u + rowLength+1)*4;
 			
-			wireframeVerts[vertPos] = rowLength + 2;
-			wireframeVerts[vertPos+1] = u + rowLength + 2;
+			wireframeVerts[vertPos] = (2*(rowLength+1))+1;
+			wireframeVerts[vertPos+1] = u + rowLength + 1;
 				
-			wireframeVerts[vertPos+2] = u + rowLength + 2;
-			wireframeVerts[vertPos+3] = u + rowLength + 3;
+			wireframeVerts[vertPos+2] = u + rowLength + 1;
+			wireframeVerts[vertPos+3] = u + rowLength + 2;
+			
+			//Vertical lines
+			int linePos = (u + 2*(rowLength+1))*4;
+			
+			wireframeVerts[linePos] = u;
+			wireframeVerts[linePos+1] = u + rowLength + 1;
+			
+			//Vertical triangles
+			int trianglePos = (u + 2*(rowLength+1))*3;
+			
+			triangleVerts[trianglePos] = u;
+			triangleVerts[trianglePos+1] = u + rowLength + 1;
+			triangleVerts[trianglePos+2] = u + rowLength + 2;
+			
+			triangleVerts[trianglePos+3] = u + rowLength + 2;
+			triangleVerts[trianglePos+4] = u + 1;
+			triangleVerts[trianglePos+5] = u;
+			
 		}
 		setVertices(gl, vertexCoords);
 		setNormals(gl, normalCoords);
